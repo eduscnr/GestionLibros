40ea6af80956d89ef2714bd3d18481cd9e8798c5
scnredu <scnredu@gmail.com>
2024-02-16 23:06:54 +0100

    Docuementacion e implementacion de archivo error

--- src/main/java/com/gestion/gestionlibros/controller/ControladorAdministrador.java
+++ src/main/java/com/gestion/gestionlibros/controller/ControladorAdministrador.java
@@ -18,43 +18,88 @@ public class ControladorAdministrador {
     private IDao dao;
     @Autowired
     private Libro libros;
+
+    /**
+     * Método que realizar la edicion de un nuevo libro
+     * @param modelo
+     * @return uan vista JSP para editar el libro
+     */
     @GetMapping("/edicionLibros")
     public String mostrarLibrosEdicion(Model modelo){
         List<Libro> libros = dao.listarLibros();
         modelo.addAttribute("listaLibros", libros);
         return "edicionLibros";
     }
+
+    /**
+     * Método para ir al JSP de un libro nuevo
+     * @param modelo
+     * @return
+     */
     @PostMapping("/irNuevoLibro")
     public String nuevoLibro(Model modelo){
         modelo.addAttribute("libro", libros);
         return "nuevoLibro";
     }
+
+    /**
+     * Método para crear un libro nuevo
+     * @param libro
+     * @return redirge si a sido con exito al JSP de edicionLibros
+     */
     @PostMapping("/nuevoLibro")
     public String enviarLibros(@ModelAttribute("libro") Libro libro){
         if(dao.insertarNuevoLibro(libro)){
             return "redirect:/edicionLibros";
         }
-        return "error";
+        return "redirect:/error";
     }
+
+    /**
+     * Método para ver los usuarios
+     * @param model
+     * @return JSP para ver los usuarios
+     */
     @GetMapping("/administrarUsuarios")
     public String getAdministrarUsuarios(Model model){
         model.addAttribute("listaUsuario", dao.listaCliente());
         System.out.println(dao.listaCliente());
         return "verUsuario";
     }
+
+    /**
+     * Método para ir a editar un usuario
+     * @param idCliente
+     * @param modelo
+     * @return JSP para editar un usuario
+     */
     @GetMapping("/editarUsuario/{idCliente}")
     public String irEditarCliente(@PathVariable("idCliente") long idCliente, Model modelo){
         Cliente c = dao.obtenerClienteId(idCliente);
         modelo.addAttribute("cliente", c);
         return "editarUsuario";
     }
+
+    /**
+     * Método para actualizar el usuario en cuestión
+     * @param cliente
+     * @param modelo
+     * @return redirige si todo ha salido bien a la lista de usuarios
+     */
     @PostMapping("/editarUsuario")
     public String editarUsuario(@ModelAttribute("cliente") Cliente cliente, Model modelo){
         if(dao.editarUsuario(cliente)){
             return "redirect:/administrarUsuarios";
         }
-        return "error";
+        return "redirect:/error";
     }
+
+    /**
+     * Método para ir a la edicion de un libro
+     * @param idLibro
+     * @param modelo
+     * @return JSP para editar un libro
+     */
     @GetMapping("/editarLibro/{idLibro}")
     public String irAEditarLibro(@PathVariable("idLibro") long idLibro,Model modelo){
         Libro libro = dao.obtenerLibroPorId(idLibro);
@@ -66,21 +111,38 @@ public class ControladorAdministrador {
         return "editarLibro";
     }
 
+    /**
+     * Método para editar el libro
+     * @param libro
+     * @return redirige si ha salido correctamente a la lista de todos los libro de edición
+     */
+
     @PostMapping("/editarLibro")
     public String libroEditado(@ModelAttribute("libro") Libro libro){
         if(dao.insertarNuevoLibro(libro)){
             return "redirect:/edicionLibros";
         }
-        return "error";
+        return "redirect:/error";
     }
 
+    /**
+     * Método para eliminar un libro
+     * @param idLibro
+     * @return si ha salido bien, redirige a la lista de edicion de libros
+     */
     @GetMapping("/eliminar/{idLibro}")
     public String eliminarLibro(@PathVariable("idLibro") long idLibro){
         if(dao.eliminarLibro(idLibro)){
             return "redirect:/edicionLibros";
         }
-        return "error";
+        return "redirect:/error";
     }
+
+    /**
+     * Método que muesta todos las venta/detalles venta de todos los clientes
+     * @param modeloCliente
+     * @return JSP que muestra esos datos
+     */
     @GetMapping("/mostrarUsuariosyDatos")
     public String mostrarUsuariosyDatos(Model modeloCliente){
         List<Venta> listaVenta = dao.listarVentaes();
@@ -88,6 +150,13 @@ public class ControladorAdministrador {
         modeloCliente.addAttribute("fechasVenta", listaVenta);
         return "mostrarClientesyDatos";
     }
+
+    /**
+     * Método que realiza la busqueda por una fecha y te devuelve todas las ventas/detalles ventas
+     * @param fechaVenta
+     * @param modelo
+     * @return JSP de todas las ventas/detalles ventas
+     */
     @GetMapping("/mostrarDetallesVenta")
     public String mostrarDetallesVentaFecha(@RequestParam("fechaVenta") String fechaVenta, Model modelo){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Formato de fecha esperado
--- src/main/java/com/gestion/gestionlibros/controller/ControladorUsuario.java
+++ src/main/java/com/gestion/gestionlibros/controller/ControladorUsuario.java
@@ -17,6 +17,13 @@ public class ControladorUsuario {
     @Autowired
     private Cliente cliente;
     private List<Carrito> listaCarrito = new ArrayList<>();
+
+    /**
+     * Método para agregar un libro a la cesta
+     * @param carrito
+     * @param model
+     * @return JSP de ver todos los libros
+     */
     @PostMapping("/agregarAlCarrito")
     public String agregarAlCarrito(@ModelAttribute("elementosCarrito") Carrito carrito, Model model) {
         boolean existe = false;
@@ -31,6 +38,13 @@ public class ControladorUsuario {
         }
         return "redirect:/verLibros";
     }
+
+    /**
+     * Método para ver todos los libros
+     * @param modelo
+     * @param session
+     * @return JSP que muesta todos los libros
+     */
     @RequestMapping("/verLibros")
     public String verLibros(Model modelo, HttpSession session){
         List<Libro> listaLibros = dao.listarLibros();
@@ -40,11 +54,24 @@ public class ControladorUsuario {
         modelo.addAttribute("carritoVacio", carritoVacio);
         return "listarLibros";//JSP
     }
+
+    /**
+     * Método para ir a registrar un nuevo usuario
+     * @param model
+     * @return
+     */
     @GetMapping("/registrar")
     public String irFormulario(Model model){
         model.addAttribute("cliente", cliente);
         return "registro"; //JSP
     }
+
+    /**
+     * Método que agregar un nuevo usuario de tipo Usuario
+     * @param cliente
+     * @param sesion
+     * @return redirige al JSP de iniciar sesión
+     */
     @PostMapping("/registrar")
     public String agregarUsuario(@ModelAttribute("cliente") Cliente cliente, HttpSession sesion){
         if(dao.agregarCliente(cliente)){
@@ -52,9 +79,16 @@ public class ControladorUsuario {
             sesion.setAttribute("rol", cliente.getRol());
             return "redirect:/";//JSP
         }else{
-            return "error";
+            return "redirect:/error";
         }
     }
+
+    /**
+     * Método que muestea la informacion de un libro
+     * @param idLib
+     * @param modelo
+     * @return JSP para mostrar la información de ese libro
+     */
     @GetMapping("/mostrarInformacion/{idLibro}")
     public String mostrarLibro(@PathVariable("idLibro") long idLib, Model modelo){
         Libro l = dao.obtenerLibroPorId(idLib);
@@ -62,16 +96,27 @@ public class ControladorUsuario {
             modelo.addAttribute("libro", l);
             return "mostrarMasInformacion";//JSP
         }else{
-            return "error";//JSP
+            return "redirect:/error";//JSP
         }
     }
 
+    /**
+     * Método que te muestra el inicio de sision
+     * @param modelo
+     * @return JSP de inicio de sision
+     */
     @GetMapping("/")
     public String mostrarInicioSesion(Model modelo){
         modelo.addAttribute("inicioUsuario", cliente);
         modelo.addAttribute("cliente", cliente);
         return "inicioSesion";//JSP
     }
+
+    /**
+     * Método que ha un logout de la página
+     * @param session
+     * @return JSP de inicio de sision
+     */
     @GetMapping("/logout")
     public String logout(HttpSession session){
         listaCarrito.clear();
@@ -80,6 +125,13 @@ public class ControladorUsuario {
         cliente = new Cliente();
         return "redirect:/";
     }
+
+    /**
+     * Método que realiza el inicio de sision si el usuario existe
+     * @param cliente
+     * @param sesion
+     * @return JSP para ver todos los libros
+     */
     @PostMapping("/")
     public String iniciarSesion(@ModelAttribute("inicioUsuario") Cliente cliente, HttpSession sesion){
         Cliente clienteEncotrado = dao.buscarCliente(cliente);
@@ -89,26 +141,40 @@ public class ControladorUsuario {
             this.cliente = clienteEncotrado;
             return "redirect:/verLibros";
         }else{
-            return "error";//JSP
+            return "inicioSesion";//JSP
         }
     }
 
+    /**
+     * Método para ver el carrito
+     * @param model
+     * @return JSP que te muestra el carrito
+     */
     @GetMapping("/verCarrito")
     public String verCarrito(Model model){
         model.addAttribute("listaCarrito",listaCarrito);
         return "verCarrito";
     }
 
+    /**
+     * Método que realiza el pedido
+     * @return JSP para ver de nuevo todos los libros
+     */
     @PostMapping("/realizarCompra")
     public String realizarPedido(){
         if(dao.ingresarVenta(cliente, listaCarrito)){
             listaCarrito.clear();
             return "redirect:/verLibros";
         }else{
-            return "error";//JSP
+            return "redirect:/error";//JSP
         }
     }
 
+    /**
+     * Método para ver el historial de compra de ese usuario logeado
+     * @param model
+     * @return JSP que te muestra el historial de compra
+     */
     @GetMapping("/verHistorialCompra")
     public String consultarHistorialCompra(Model model) {
         List<DetallesVenta> ventasRealizadas = new ArrayList<>();
@@ -121,6 +187,11 @@ public class ControladorUsuario {
         return "historialCompras";
     }
 
+    /**
+     * Método que te lista los libros por genero
+     * @param model
+     * @return JSP ver los generos
+     */
     @GetMapping("/verGeneros")
     public String listarGeneros(Model model){
         List<Genero> generos = dao.listarGeneros();
@@ -128,6 +199,13 @@ public class ControladorUsuario {
         model.addAttribute("listaTodosLosGeneros", generos);
         return "verCategorias";
     }
+
+    /**
+     * Método que te muestra el libro buscado
+     * @param titulo
+     * @param model
+     * @return JSP de listar de nuevo todos los libro con ese nombre a buscar
+     */
     @GetMapping("/buscarLibros")
     public String buscarLibros(@RequestParam("q") String titulo, Model model) {
         List<Libro> librosEncontrados = dao.bucarLibro(titulo);
@@ -135,6 +213,11 @@ public class ControladorUsuario {
         return "listarLibros";
     }
 
+    /**
+     * Método que te elimina un libro del carrito
+     * @param idLibro
+     * @return JSP de ver el carrtio de nuevo
+     */
     @GetMapping("/eliminarElemento/{idLibro}")
     public String eliminarElementoCarrito(@PathVariable("idLibro") long idLibro){
         Carrito eliminar = null;
@@ -146,6 +229,13 @@ public class ControladorUsuario {
         listaCarrito.remove(eliminar);
         return"redirect:/verCarrito";
     }
+
+    /**
+     * Método que te filtra según el genero seleccionado
+     * @param genero
+     * @param modelo
+     * @return JSP para mostrar el genero seleccionado
+     */
     @GetMapping("/mostrarLibrosPorGenero")
     public String mostrarLibrosGenero(@RequestParam("generoSeleccionado") String genero, Model modelo){
         List<Genero> listaLibroPorGenero = dao.buscarPorGenero(genero);
@@ -154,6 +244,14 @@ public class ControladorUsuario {
         modelo.addAttribute("listaGenero", listaLibroPorGenero);
         modelo.addAttribute("listaTodosLosGeneros", listarGeneros);
         return "verCategorias";
+    }
+    @RequestMapping("/error")
+    public String mostrarError(){
+        return "error";
+    }
+    @RequestMapping("/errorInicioSesion")
+    public String mostrarErrorInicioSesion(){
+        return "errorInicioSesion";
     }
 
 }
